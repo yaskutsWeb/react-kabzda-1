4520d6a7e82c0133329000bff485465d05e4e198
Pavel <kaptowka1999@mail.ru>
2022-03-12 09:04:56 +0300

    basic components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img className="logotype-image"
+				 src="https://автолого.рф/wp-content/uploads/bmw-logo-2020-grey.png"
+				 alt="LogoType"/>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+	return (
+		<main className="content">
+			<img className="content-header-image"
+				 src="https://www.theautohost.com/_contentPages/vehicleContentPages/BMW/2022/X4/images/2022-BMW-X4-header.jpg"
+				 alt="BMW"/>
+			<div>
+				<img
+					src="https://st3.depositphotos.com/1083585/18173/i/600/depositphotos_181732652-stock-photo-bmw-logotype-insignia.jpg"
+					alt="BMW-Hi"/>
+				+description
+			</div>
+			<div>
+				my posts
+				<div>new post</div>
+				<div>
+					post1
+				</div>
+				<div>
+					post2
+				</div>
+			</div>
+		</main>
+	);
+}
+
+export default Profile;
